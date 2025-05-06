4ae4633851dff56aa88c2b008bea96c79a0481e5
miklymchuk <miklymchuk@gmail.com>
2025-05-05 20:08:26 -0400

    Added Truck and Bike classes.

--- src/main/java/org/example/Bike.java
+++ src/main/java/org/example/Bike.java
@@ -1,4 +1,62 @@
 package org.example;
 
-public class Bike {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Bike extends Vehicle implements Pricing {
+    Boolean isElectric;
+
+    public Bike(String model, String quality, Boolean isElectric) {
+        super(model, quality);
+        this.isElectric = isElectric;
+    }
+
+    @Override
+    public double calculateCost() {
+        double cost = isElectric ? 100 : 80;
+
+        switch (quality) {
+            case "low" -> cost*= 0.8;
+            case "high" -> cost*= 1.5;
+            case "perfect" -> cost*= 2;
+            default -> cost*= 1;
+        }
+
+        return cost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Bike bike = (Bike) o;
+        return Objects.equals(isElectric, bike.isElectric);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), isElectric);
+    }
+
+    @Override
+    public String toString() {
+        return "Bike{" +
+                "isElectric=" + isElectric +
+                "} " + super.toString();
+    }
+
+    public Boolean getElectric() {
+        return isElectric;
+    }
+
+    public void setElectric(Boolean electric) {
+        isElectric = electric;
+    }
+
+    public static class CostComparator implements Comparator<Bike> {
+        @Override
+        public int compare(Bike o1, Bike o2) {
+            return (int) (o1.calculateCost() - o2.calculateCost());
+        }
+    }
 }
--- src/main/java/org/example/Truck.java
+++ src/main/java/org/example/Truck.java
@@ -1,4 +1,67 @@
 package org.example;
 
-public class Truck {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Truck extends Vehicle implements Pricing {
+    String size;
+
+    public Truck(String model, String quality, String size) {
+        super(model, quality);
+        this.size = size;
+    }
+
+    public double calculateCost() {
+        double cost = 250;
+
+        switch (size) {
+            case "small" -> cost*= 0.6;
+            case "large" -> cost*= 1.5;
+            default -> cost*=1;
+        }
+
+        switch (quality) {
+            case "low" -> cost*= 0.8;
+            case "high" -> cost*= 1.15;
+            case "perfect" -> cost*= 1.4;
+            default -> cost*= 1;
+        }
+
+        return cost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Truck truck = (Truck) o;
+        return Objects.equals(size, truck.size);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), size);
+    }
+
+    @Override
+    public String toString() {
+        return "Truck{" +
+                "size='" + size + '\'' +
+                "} " + super.toString();
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public static class CostComparator implements Comparator<Truck> {
+        @Override
+        public int compare(Truck o1, Truck o2) {
+            return (int) (o1.calculateCost() - o2.calculateCost());
+        }
+    }
 }
