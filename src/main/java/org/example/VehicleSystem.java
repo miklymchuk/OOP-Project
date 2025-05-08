3a522a8db07fe043946be8879b5c6fef54aa7ccc
Michael Klymchuk <6312398@edu.vaniercollege.qc.ca>
2025-05-08 15:41:10 -0400

    Completed filter() method.

--- src/main/java/org/example/VehicleSystem.java
+++ src/main/java/org/example/VehicleSystem.java
@@ -31,19 +31,15 @@ public class VehicleSystem {
         Map<Vehicle, Double> filteredVehicles = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
 
-
         if (key % 10 == 1) {
-            List<Vehicle> a = vehicles.get("car");
+            List<Vehicle> carVehicles = vehicles.get("car");
             List<Car> cars = new ArrayList<>();
 
-            for (Vehicle v : a) {
-                if (v instanceof Car) {
-                    cars.add((Car) v);
-                }
+            for (Vehicle v : carVehicles) {
+                cars.add((Car) v);
             }
 
-            key = key/= 10;
-
+            key /= 10;
 
             if (key % 10 == 1) {
                 cars.sort(new Car.CostComparator());
@@ -55,8 +51,9 @@ public class VehicleSystem {
 
             } else if (key % 10 == 2) {
                 Collections.sort(cars);
+
                 String quality = "";
-                while (!((quality == "low") || (quality == "medium") || (quality == "high") || (quality == "perfect"))) {
+                while (!((quality.equals("low")) || (quality.equals("medium")) || (quality.equals("high")) || (quality.equals("perfect")))) {
                     quality = scanner.nextLine();
                 }
 
@@ -67,7 +64,7 @@ public class VehicleSystem {
             } else if (key % 10 == 3) {
                 Collections.sort(cars);
                 String size = "";
-                while (!((size == "small") || (size == "medium") || (size == "large"))) {
+                while (!((size.equals("small")) || (size.equals("medium")) || (size.equals("large")))) {
                     System.out.println("Enter the size of car you are looking for.");
                     size = scanner.nextLine();
                 }
@@ -78,7 +75,7 @@ public class VehicleSystem {
                         .forEach(car -> filteredVehicles.put(car, car.calculateCost()));
             } else {
                 Collections.sort(cars);
-                key/= 10;
+                key /= 10;
 
                 boolean electric;
                 if (key % 10 == 1) {
@@ -92,9 +89,90 @@ public class VehicleSystem {
                         .forEach(car -> filteredVehicles.put(car, car.calculateCost()));
             }
         } else if (key % 10 == 2) {
+            List<Vehicle> truckVehicles = vehicles.get("truck");
+            List<Truck> trucks = new ArrayList<>();
 
+            for (Vehicle v : truckVehicles) {
+                trucks.add((Truck) v);
+            }
+            key /= 10;
+
+            if (key % 10 == 1) {
+                trucks.sort(new Truck.CostComparator());
+
+                int maxCost = scanner.nextInt();
+                trucks.stream()
+                        .filter(truck -> truck.calculateCost() <= maxCost)
+                        .forEach(truck -> filteredVehicles.put(truck, truck.calculateCost()));
+            } else if (key % 10 == 2) {
+                Collections.sort(trucks);
+
+                String quality = "";
+                while (!((quality.equals("low")) || (quality.equals("medium")) || (quality.equals("high")) || (quality.equals("perfect")))) {
+                    quality = scanner.nextLine();
+                }
+
+                String finalQuality = quality;
+
+                trucks.stream()
+                        .filter(truck -> truck.getQuality().equals(finalQuality))
+                        .forEach(truck -> filteredVehicles.put(truck, truck.calculateCost()));
+            } else {
+                Collections.sort(trucks);
+                String size = "";
+                while (!((size.equals("small")) || (size.equals("medium")) || (size.equals("large")))) {
+                    System.out.println("Enter the size of car you are looking for.");
+                    size = scanner.nextLine();
+                }
+
+                String finalSize = size;
+                trucks.stream()
+                        .filter(truck -> truck.getSize().equals(finalSize))
+                        .forEach(truck -> filteredVehicles.put(truck, truck.calculateCost()));
+            }
         } else {
+            List<Vehicle> bikeVehicles = vehicles.get("bike");
+            List<Bike> bikes = new ArrayList<>();
 
+            for (Vehicle v : bikeVehicles) {
+                bikes.add((Bike) v);
+            }
+            key /= 10;
+
+            if (key % 10 == 1) {
+                bikes.sort(new Bike.CostComparator());
+
+                int maxCost = scanner.nextInt();
+                bikes.stream()
+                        .filter(bike -> bike.calculateCost() <= maxCost)
+                        .forEach(bike -> filteredVehicles.put(bike, bike.calculateCost()));
+            } else if (key % 10 == 2) {
+                Collections.sort(bikes);
+
+                String quality = "";
+                while (!((quality.equals("low")) || (quality.equals("medium")) || (quality.equals("high")) || (quality.equals("perfect")))) {
+                    quality = scanner.nextLine();
+                }
+
+                String finalQuality = quality;
+
+                bikes.stream()
+                        .filter(bike -> bike.getQuality().equals(finalQuality))
+                        .forEach(bike -> filteredVehicles.put(bike, bike.calculateCost()));
+            } else {
+                key /= 10;
+
+                boolean electric;
+                if (key % 10 == 1) {
+                    electric = true;
+                } else {
+                    electric = false;
+                }
+
+                bikes.stream()
+                        .filter(bike -> bike.getElectric() == electric)
+                        .forEach(bike -> filteredVehicles.put(bike, bike.calculateCost()));
+            }
         }
         return filteredVehicles;
     }
